de3fa16a8d3bdc32814193fe9a0adf3cc3d8a00b
Artem Gavryushin <90931383+Artem27Rezt@users.noreply.github.com>
2022-07-20 18:39:21 +0300

    Update default-scripts.js

--- scripts/default-scripts.js
+++ scripts/default-scripts.js
@@ -4,7 +4,9 @@ window.addEventListener('DOMContentLoaded', () => {
  		  manMenu = document.querySelector('.man'),
 		  menu = document.querySelector('.second_level'),
           fullMenu = document.querySelector('.third_level'),
-          menuPhoto = document.querySelector('.image_watch_all');
+          menuPhoto = document.querySelector('.image_watch_all'),
+          iconUser = document.querySelector('.icon_user'),
+          extraUserData = document.querySelector('.extra_user_data');
 
     function thirdLevelMenu(link){
 		menuPhoto.src = link;
@@ -20,6 +22,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	manMenu.addEventListener('mouseover', () => {
 		thirdLevelMenu('image/watch_all_promotions_man.webp');
+	});
+
+	iconUser.addEventListener('mouseover', () => {
+		extraUserData.classList.toggle('hide');
+		extraUserData.onmouseleave = function(e){ extraUserData.classList.toggle('hide'); }
 	});
 
 });
